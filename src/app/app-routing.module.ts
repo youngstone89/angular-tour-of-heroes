c6f654d293c1126fb3db73724600bacd61c080bf
youngstone <youngstone89@gmail.com>
2018-11-12 21:38:21 +0900

    Item Directive Added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import { HeoresComponent } from './heores/heores.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
+import {HeroDetailComponent} from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   { path: 'heroes', component: HeoresComponent },
-  { path: 'dashboard', component: DashboardComponent}
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'detail/:id', component: HeroDetailComponent}
 ];
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,19 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ItemDirective } from './item.directive';
 
 @NgModule({
+  // These components are to be solely used in the module unless they are exported and imported.
   declarations: [
     AppComponent,
     HeoresComponent,
     HeroDetailComponent,
     MessagesComponent,
     DashboardComponent,
+    ItemDirective,
   ],
+  // This particular modules should function properly
   imports: [
     BrowserModule,
     FormsModule,
